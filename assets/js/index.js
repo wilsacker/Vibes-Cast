3dc8a5cebffdcb6b20d2abfd646c6172e3121fb1
Lenin <Leninmartinezvilla@gmail.com>
2024-06-02 20:08:59 -0400

    made more changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,7 +35,7 @@ function getWeatherData(lat, lon) {
         .then(response => response.json())
         .then(data => {
             displayWeatherData(data);
-            console.log(data)
+            console.log("Weather Data: ", data)
         })
         .catch(error => console.error('Error fetching weather data:', error));
 }
@@ -136,7 +136,7 @@ async function getAccessToken(clientId, clientSecret) {
 }
 
 async function getJazzSongs(accessToken) {
-    const result = await fetch('https://api.spotify.com/v1/search?q=genre:jazz&type=track&limit=5', {
+    const result = await fetch(`https://api.spotify.com/v1/search?q=genre:jazz&type=track&limit=5`, {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + accessToken
@@ -147,7 +147,7 @@ async function getJazzSongs(accessToken) {
     return data.tracks.items;
 }
 
-async function main() {
+async function main(weatherCode) {
     const clientId = '85aa1b8ce49b49eb87d1cfe0ba9b3f96';
     const clientSecret = '2dd9402bc04447cd917e5c5f513603a3';
 
